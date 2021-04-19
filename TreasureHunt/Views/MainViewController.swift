c8e6dc579320f19550f26c29331ebe1a91761dd5
ejayk <estherkim0907@gmail.com>
2021-04-18 22:24:50 -0400

    Bike/Drive showing. Small errors removed.

--- TreasureHunt/Views/MainViewController.swift
+++ TreasureHunt/Views/MainViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 import SwiftUI
 import MapKit
+import Firebase
 
 class MainViewController: UIViewController, CLLocationManagerDelegate{
-    
+    let geocoder = CLGeocoder()
+
     var locationManager: CLLocationManager!
+    var mapView: MKMapView!
     
     @IBOutlet var menuBtns: [UIButton]!
+    
+    @IBAction func flagTogglebtn(_ sender: Any) {
+        // when enabled, shows all flags
+        // disabled hides flags
+    }
     
     @IBAction func handle(_ sender: UIButton) {
         menuBtns.forEach { (button) in
@@ -30,6 +38,24 @@ class MainViewController: UIViewController, CLLocationManagerDelegate{
         
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
-    }
-    
-}
+        
+        /*
+        let ref = Database.database().reference()
+                let flagsRef = ref.child("flags")
+        flagsRef.observeSingleEvent(of: .value, with: { (snapshot) in
+                    for snap in snapshot.children {
+                        let postSnap = snap as! DataSnapshot
+                        if let dict = postSnap.value as? [String:AnyObject] {
+                            let name = dict["name"] as! String
+                            let address = dict["address"] as! String
+                            let task = dict["task"] as! String
+                            let tags = dict["tags"] as! String
+                            let rating = dict["rating"] as! Double
+                            let coordinate = CLLocationCoordinate2D(latitude: dict["lat"] as! Double, longitude: dict["long"] as! Double)
+                            let flag = Flag(name: name, address: address, task: task, tags: tags, description: description, rating: rating)
+                            self.mapView.addAnnotation(flag)
+                        }
+                    }
+                })
+ */
+    }}
